447f07336943cd719a24d22eac95f437c74cd7db
voodootuna <jahnot@gmail.com>
2015-09-30 00:19:45 +0400

    added j_form plugin

---
+++ wp-content/plugins/jb-form.php
@@ -0,0 +1,24 @@
+<?php
+/*
+Plugin Name: FORM PLUGIN
+Plugin URI: http://www.jarboo.com
+Description: FORM PLGIN
+Version: 0.1 
+Author: Jarboo
+Author URI: http://www.jarboo.com
+*/
+
+function j_handler() {
+
+$form_content = '<div id="j_form" class="j_form">
+<form>
+First name: <input type="text" name="firstname"><br>
+Last name: <input type="text" name="lastname"><br>
+Message: <textarea name="message"> Enter text here...</textarea>
+</form>
+</div>';
+return $form_content;
+  }
+
+add_shortcode("j_form", "j_handler");
+?>
